429413730cf807602e4379a003e8687cc02d7b48
Patrick Ashi <patrickpeko1@gmail.com>
2025-03-01 22:16:12 +0100

    tawk.to api intg

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -52,7 +52,14 @@ const  Navbar = ({ handleContactClick }) => {
             <div className={nav ? "fixed left-0 top-0 h-full w-[90%] ease-out duration-500 bg-gray-100" : "fixed left-[-100%]"}>
 
                 <div className="flex align-middle mt-4 ml-4 ">
-                    <img className="w-14 rounded-full" src={pekodev} size={20} alt="logo" />
+                    {/* <img className="w-14 rounded-full" src={pekodev} size={20} alt="logo" /> */}
+                    <div className="flex align-middle ">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-[#1e5036] font-semibold align-middle ">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
+                        </svg>
+
+                        <h2 className="text-[#1e5036] font-semibold text-xl ml-2 mt-1">FARMSPACE</h2>
+                    </div>
                     
                 </div>
 
