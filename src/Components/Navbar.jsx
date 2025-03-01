b5884e59ea8bf14eba73574b308a9d1119bd926f
Patrick Ashi <patrickpeko1@gmail.com>
2025-03-01 23:00:10 +0100

    new

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react"
 import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai"
 // import blackbullLogo from "../Assets/blackbullLogo.jpg"
-import pekodev from "../Assets/pekodev.webp";
+import farmspacelogo_bw from "../Assets/farmspacelogo_bw.png";
 
 
 const  Navbar = ({ handleContactClick }) => {
@@ -21,14 +21,8 @@ const  Navbar = ({ handleContactClick }) => {
     return ( 
         <div className="flex justify-between items-center fixed top-0 left-0 right-0 z-50 py-2 bg-[#93b59f] px-2 md:px-6 text-white font-Montserrat ">
             <div>
-                {/* <img className="w-14 rounded-full" src={pekodev} size={20} alt="logo" /> */}
-                <div className="flex align-middle ">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-white font-semibold align-middle ">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
-                    </svg>
-
-                    <h2 className="text-white font-semibold text-xl ml-2 mt-1">FARMSPACE</h2>
-                </div>
+                <img className="w-28" src={farmspacelogo_bw} size={20} alt="logo" />
+      
             </div>
             
             <div>
@@ -52,14 +46,7 @@ const  Navbar = ({ handleContactClick }) => {
             <div className={nav ? "fixed left-0 top-0 h-full w-[90%] ease-out duration-500 bg-gray-100" : "fixed left-[-100%]"}>
 
                 <div className="flex align-middle mt-4 ml-4 ">
-                    {/* <img className="w-14 rounded-full" src={pekodev} size={20} alt="logo" /> */}
-                    <div className="flex align-middle ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 text-[#1e5036] font-semibold align-middle ">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
-                        </svg>
-
-                        <h2 className="text-[#1e5036] font-semibold text-xl ml-2 mt-1">FARMSPACE</h2>
-                    </div>
+                    <img className="w-28" src={farmspacelogo_bw} size={20} alt="logo" />
                     
                 </div>
 
--- src/Components/Secone.jsx
+++ src/Components/Secone.jsx
@@ -7,10 +7,10 @@ const Secone = () => {
     return (
         <div className="flex justify-center text-center mx-4 md:mx-20 py-20 mt-10 mb-12 text-gray-100">
             <div ref={ref} className={`w-full md:w-[600px]  transition-transform duration-100 ${isVisible ? 'animate-slideIn' : '-translate-x-1'}`}>
-                <h2 className="text-5xl md:text-6xl mt-8">Farm Irrigation System You Can Count On</h2>
+                <h2 className="text-5xl md:text-6xl mt-8">AFFORDABLE SOIL TESTING SERVICES</h2>
                 <p className="my-6 text-xl">
-                    We combat climate change through mitigation and adaptation, removing carbon  and building
-                    climate resilient communities and landscapes 
+                    We provide access to soil evaluation as a service in real time to empower smallholder farmers
+                    with data-driven insights to make informed agronomic decisions. 
                 </p>
                 <button className="bg-white text-gray-800 hover:bg-[#1e5036] hover:text-white duration-200 px-4 rounded-full py-2 font-semibold">
                     How it Works
