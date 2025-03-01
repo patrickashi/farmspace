ad1087fc5cd94969d99a28e680f5b2a7e4bb8390
Patrick Ashi <patrickpeko1@gmail.com>
2025-03-01 16:00:37 +0100

    new

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <div className="flex flex-col md:flex-col lg:flex-row gap-12 px-4 md:px-20 pt-20  duration-200 bg-[#1B2F27] border-t border-gray-300">
             <div ref={ref} className={`w-full md:w-[700px] transition-transform duration-100 ${isVisible ? 'animate-slideIn' : '-translate-x-1'}`}>
                 <button className="border rounded-tr-xl px-4 py-1 text-xs font-mono font-semibold bg-gray-100 text-gray-800">ABOUT</button>
-                <h2 className="mt-4 mb-8 text-3xl md:text-4xl font-mono text-gray-200">All About Belsurance</h2>
+                <h2 className="mt-4 mb-8 text-3xl md:text-4xl font-mono text-gray-200">All About Farmspace</h2>
             </div>
 
             <div className="custom-scrollbar flex flex-col h-80 overflow-y-scroll  justify-between gap-4 py-10 text-gray-200 text-md mb-4 md:mb-20 ">
